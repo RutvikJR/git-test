bd8c47db21277420f286104e924df783a6a8adfe
Rutvik Rachhadiya <rachhadiyarutvik09040@gmail.com>
2024-11-03 22:05:00 +0530

    commit 1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,6 +18,7 @@ const Home = () => {
     <div>
       <div className="m-2 text-xl font-semibold">Page: home</div>
       <div className="m-2 text-xl font-semibold">counter</div>
+      <div className="m-2 text-xl font-semibold">test 1</div>
       <div className="m-2 text-xl font-semibold">{count}</div>
       <button
         className="p-4 bg-cyan-600 text-white m-2 rounded-lg"
